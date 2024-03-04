b895dc8912729476584214278cb16315a377c5c6
zhorogs <zgslavchev@gmail.com>
2024-03-04 21:28:31 +0200

    working with jshell

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,3 +3,41 @@ class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+// working in jshell, but moving here
+/* start commenting
+
+1. Statements.
+
+It's a complete command to be executed. It can include one or more expressions.
+
+Example:
+System.out.print("Hello World");
+
+Challenge:
+
+System.out.print("Hello, Zhoro.");
+
+end commenting
+
+2. Variables and keywords.
+
+A keyword is any one of a number of reserved words, that have a predefined meaning in the Java language.
+
+In Java syntax, all code is case-sensitive, and this includes keywords.
+
+https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+
+What are variables?
+
+Variables are a way to store information in our computer.
+Variables that we define in a program, can be accessed by a name we give them, and the computer does the hard work,
+of figuring out where they get stored, in the computer's random access memory, or RAM.
+
+There are lots of different types of data, that we can define for our variables, collectively known as data types.
+
+Some data types are keywords in Java.
+
+int myFirstNumber = 5;
+
+ */
